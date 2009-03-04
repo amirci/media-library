481da39d6fd1d1330fe239236c63855ac3ecf302
amirci <amir@barylko.com>
2009-03-03 22:32:36 -0600

    Replaces tabs by spaces

--- db/migrate/20090304034324_create_movies.rb
+++ db/migrate/20090304034324_create_movies.rb
@@ -1,9 +1,9 @@
 class CreateMovies < ActiveRecord::Migration
   def self.up
     create_table :movies do |t|
-	t.string :title, :null => false
-	t.string :directors, :actors
-	t.date :release_date
+      t.string :title, :null => false
+      t.string :directors, :actors
+      t.date :release_date
       t.timestamps
     end
   end
