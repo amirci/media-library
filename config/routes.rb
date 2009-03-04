ec31a065ed81f3142e19089e56053fcbaec4236a
amirci <amir@barylko.com>
2009-03-03 22:24:37 -0600

    Added missing files
    	modified:   config/routes.rb
    	app/controllers/movies_controller.rb
    	app/helpers/movies_helper.rb
    	app/models/
    	config/.gitignore
    	db/
    	features/
    	lib/
    	log/.gitignore
    	script/cucumber
    	spec/

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id.:format'
+
+  map.resources :movies
 end
