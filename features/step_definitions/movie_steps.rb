1bdffd8998853a7acb038fe381ff8e513328c69f
amirci <amir@barylko.com>
2009-03-04 21:44:02 -0600

    Added views for index and layout Changed the rule to match the steps

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,5 +1,5 @@
 When /^the (\d+)(st|nd|rd|th) movie has title "(.*)"$/ do |number, ordinal, title|
-	response.should have_tag(".movies .movie:nth-child(#{number} .title)", title)
+	response.should have_tag(".movies.movie:nth-child(#{number}).title", title )
 end
 
 When /^that the library has the following movies:$/ do |movies|
